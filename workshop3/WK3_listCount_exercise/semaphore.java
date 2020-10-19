51615f3b0c9f367a427fc0fd6f7b58f9cf3fb5f1
NaseelaAmboorallee <60098696+NaseelaAmboorallee@users.noreply.github.com>
2020-10-19 08:47:59 +0100

    WkSp3: Written semaphore class that compiles.

--- workshop3/WK3_listCount_exercise/semaphore.java
+++ workshop3/WK3_listCount_exercise/semaphore.java
@@ -1,6 +1,29 @@
 import java.util.*;
 public class semaphore
 {
+    private int count = 0;
+
+    public semaphore(int init_val){
+        if (init_val >= 0) count = init_val;
+    }
+
+    public synchronized void P() {
+        count = count - 1;
+        if (count < 0) {
+            try{
+                wait();
+            } catch(Exception e){ }
+        }
+    }
+
+    public synchronized void V(){
+        count = count + 1;
+        if (count <= 0) {
+            try{
+                notify();
+            } catch(Exception e){ }
+        }
+    }
 
 }
 
