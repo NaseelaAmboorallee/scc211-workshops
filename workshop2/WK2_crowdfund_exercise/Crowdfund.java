b98fd0ff2bf4ceb79eb2c3483d6586371e13274a
NaseelaAmboorallee <60098696+NaseelaAmboorallee@users.noreply.github.com>
2020-10-26 13:18:47 +0000

    WkSp2: Added Threads to driver class.

--- workshop2/WK2_crowdfund_exercise/Crowdfund.java
+++ workshop2/WK2_crowdfund_exercise/Crowdfund.java
@@ -7,21 +7,43 @@ import java.lang.*;
 
 public class Crowdfund { 
   /* The first class called in the program - creates number of donators, predicted funds, and final outcome*/
-   public static void main (String arg[]) 
+   public static void main (String arg[]) throws InterruptedException 
    {
-     Scanner input = new Scanner(System.in);
-     Random rand = new Random();      								
+     Scanner input = new Scanner(System.in);   
+     ArrayList<Thread> t = new ArrayList<Thread>();
+     ArrayList<funder> f = new ArrayList<funder>();
      int predictedDonations = 0;			//Variable declarations that can be potentially use
      int numUsers = 0;
-     
+ 
      int donation = 5;						//The amount (�) that donators are able to contribute
+     int actualDonations = 0;
      
      System.out.println("Please enter the total number of backers: ");
      numUsers = input.nextInt();
      
      predictedDonations = numUsers * donation;
      
-     input.close(); 
+     input.close();
+
+     for (int i = 0;i < numUsers ; i++){
+      funder donator = new funder();
+      Thread t1 = new Thread(donator);
+      f.add(donator);
+      t.add(t1);
+     }
+     
+     for (Thread thread : t){
+      thread.start();
+     }
+
+     for (Thread thread : t){
+      thread.join();
+     }
+
+     actualDonations = funder.account.readAccount();
+
+ 
+
      /*The developer wants to implement three things: 
      
      	1) Allow the user to specify the number of donators participating in the crowd sourcing
@@ -37,6 +59,7 @@ public class Crowdfund {
      System.out.println("Num. of Contributors: " + numUsers);
      System.out.println("----------------");
      System.out.println("Predicted Donations: " + predictedDonations);
+     System.out.println("Actual Donations: " + actualDonations);
    
 	}																				
 }
