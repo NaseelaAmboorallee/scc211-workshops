74e872f61b532c9da7bbcccdb13c2e485b44c249
NaseelaAmboorallee <60098696+NaseelaAmboorallee@users.noreply.github.com>
2020-10-26 12:56:26 +0000

    WkSp2: Implemented Run interface in funder class.

--- workshop2/WK2_crowdfund_exercise/funder.java
+++ workshop2/WK2_crowdfund_exercise/funder.java
@@ -5,14 +5,15 @@ This exercise you will show an implementation of a lock (mutex)*/
 import java.util.*;
 import java.lang.*;
 
-public class funder{ 
+public class funder implements Runnable{ 
   /*The donator is responsible for depositing their donation into the bank account */
   
-  public static deposit account;			//Would advise that you leave this line of code here
+  public static deposit account = new deposit();			//Would advise that you leave this line of code here
    			 
   											//Hint, you will want to implement one of the deposit methods in here somewhere...
-   
-  
+  public void run(){
+      account.deposit_funds(5);
+  } 
   
   }    
 
