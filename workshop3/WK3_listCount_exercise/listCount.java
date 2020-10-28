c775bc98e715002deb69987ee4aa032d93b16d4e
NaseelaAmboorallee <60098696+NaseelaAmboorallee@users.noreply.github.com>
2020-10-28 16:07:15 +0000

    Added threads to driver class.

--- workshop3/WK3_listCount_exercise/listCount.java
+++ workshop3/WK3_listCount_exercise/listCount.java
@@ -1,11 +1,12 @@
 import java.util.*;
 
 public class listCount { 
-  ArrayList<Thread> t = new ArrayList<Thread>();
-  ArrayList<user> u = new ArrayList<user>();
+  
 
-public static void main(String[] args)
+public static void main(String[] args) throws InterruptedException
   {
+   ArrayList<Thread> t = new ArrayList<Thread>();
+   ArrayList<user> u = new ArrayList<user>();
    int num_users = 0;
    int elements = 0;
    int bufferSize = 0;
@@ -22,6 +23,17 @@ public static void main(String[] args)
    
    Buffer b; 	
    b = new Buffer(bufferSize);						//Create buffer
+
+   for (int i = 0;i < num_users ; i++){
+    user usr = new user(i,elements,b);
+    Thread t1 = new Thread(usr);
+    u.add(usr);
+    t.add(t1);
+   }
+   
+   for (Thread thread : t){thread.start();}
+
+   for (Thread thread : t){thread.join();}
    	
    //Create X users that will concurrently add elements to the Buffer object b
    //This can be performed by using an add() method within the Buffer class that needs to be completed.
