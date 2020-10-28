c300e8efd9ed72e6f5bf0f8db16bef9899512058
NaseelaAmboorallee <60098696+NaseelaAmboorallee@users.noreply.github.com>
2020-10-28 13:40:44 +0000

    Added user input to driver class. Code currently compiles.

--- workshop3/WK3_listCount_exercise/listCount.java
+++ workshop3/WK3_listCount_exercise/listCount.java
@@ -1,20 +1,28 @@
 import java.util.*;
 
-public class listCount {
+public class listCount { 
+  ArrayList<Thread> t = new ArrayList<Thread>();
+  ArrayList<user> u = new ArrayList<user>();
 
 public static void main(String[] args)
   {
    int num_users = 0;
    int elements = 0;
    int bufferSize = 0;
+   Scanner input = new Scanner(System.in);  
 		
-   System.out.println("Enter number of users:");			//Specify number of users			
-  			
+   System.out.println("Enter number of users:");			//Specify number of users
+   num_users = input.nextInt();
+
    System.out.println("Enter number of elements per user:");		//Specify number of elements submitted per user	
-     
+   elements = input.nextInt();
+   input.close();
+
+   bufferSize = num_users * elements;
+   
    Buffer b; 	
    b = new Buffer(bufferSize);						//Create buffer
-	
+   	
    //Create X users that will concurrently add elements to the Buffer object b
    //This can be performed by using an add() method within the Buffer class that needs to be completed.
    
